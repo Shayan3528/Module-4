1d026e68b1d58729f658a33b1424d69c07bf95b2
Shayan <35280babu@gmail.com>
2025-03-27 17:39:14 +0600

    initializing the application out of component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useEffect } from "react";
 import {
   checkAuthToken,
   loadDataFromLocalStorage,
 } from "./Utlis/getRandomInteger";
 
-let didInit = false;
-
 export default function App() {
-  useEffect(() => {
-    if (!didInit) {
-      didInit = true;
-      const a = loadDataFromLocalStorage();
-      const b = checkAuthToken();
-
-      console.log(b + " " + a);
-    }
-  }, []);
   return <></>;
 }
+
+const a = loadDataFromLocalStorage();
+const b = checkAuthToken();
+console.log(b + " " + a);
