4ea7eb51672c08eef0c15cb36d457e966390e991
Shayan <35280babu@gmail.com>
2025-03-27 17:07:39 +0600

    chains of computations using handler

--- src/Component/Example6.jsx
+++ src/Component/Example6.jsx
@@ -1,40 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getRandomBoolean, getRandomInteger } from "../Utlis/getRandomInteger";
 
 export default function Example6() {
   const [card, setCard] = useState(null);
   const [goldCardCount, setGoldCardCount] = useState(0);
   const [round, setRound] = useState(1);
-  const [isGameOver, setGameOver] = useState(false);
 
-  useEffect(() => {
-    if (card !== null && card.gold) {
-      setGoldCardCount((c) => c + 1);
-    }
-  }, [card]);
-
-  useEffect(() => {
-    if (goldCardCount > 3) {
-      setRound((r) => r + 1);
-      setGoldCardCount(0);
-    }
-  }, [goldCardCount]);
-
-  useEffect(() => {
-    if (round > 5) {
-      setGameOver(true);
-    }
-  }, [round]);
-
-  useEffect(() => {
-    if (isGameOver) console.log("Good game!");
-  }, [isGameOver]);
+  const isGameOver = round > 5;
 
   function handlePlaceCard(nextCard) {
     if (isGameOver) {
       throw Error("Game already ended.");
     } else {
       setCard(nextCard);
+      if (nextCard.gold) {
+        if (goldCardCount <= 3) {
+          setGoldCardCount(goldCardCount + 1);
+        } else {
+          setGoldCardCount(0);
+          setRound(round + 1);
+          if (round === 5) {
+            console.log("Good Game");
+          }
+        }
+      }
     }
   }
 
